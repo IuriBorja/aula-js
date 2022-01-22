5f389c3b3cbd34d87e7562b3c4bc885c34e2c3e7
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-01-22 22:15:57 +0000

    att

---
+++ script.js
@@ -0,0 +1,33 @@
+var a = 10;
+var b = 15;
+
+var c = a + b;
+
+console.log(c);
+
+
+var d = 10;
+var e = 25;
+
+var f = d * e;
+
+console.log(f);
+
+
+var g = 30;
+
+var h = g++;
+
+console.log(h);
+console.log(g);
+
+
+var carro = "ligado";
+var rua = "aberta";
+
+
+if (carro === "ligado") {
+    console.log(carro)
+} else {
+    console.log(rua)
+}
