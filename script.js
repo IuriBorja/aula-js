f5618064706a29854254353509832ccc7e052fb5
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2022-01-25 01:28:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,109 @@ if (carro === "ligado") {
 } else {
     console.log(rua)
 }
+
+
+var i = 17;
+
+i = i % 2;
+
+
+console.log(i);
+
+
+var a = 4;
+var b = 4;
+
+console.log( a != b );
+
+
+
+
+/*var idade = 29;
+
+var maior20 = idade >= 20;
+var menor30 = idade <= 30;
+
+
+var entre = maior20 && menor30;
+
+console.log("Idade", idade);
+
+console.log("Maior que vinte", maior20);
+
+console.log("Menor que trinta", menor30);
+
+console.log("Entre 20 e 30", entre);*/
+
+
+/*var idade = 8;
+
+var maior20 = idade >= 20;
+var menor20 = !maior20;
+
+console.log("Maior que vinte", maior20);
+
+console.log("Menor que vinte", menor20);*/
+
+
+/*var numero = parseInt(prompt("Digite um número"));
+
+
+
+alert("A raiz quadrada de  " + numero + " é " + numero ** 2);
+
+
+/*alert("vamos calcular a formula de baskhara para você")
+
+var a = prompt("digite o valor de A")
+var b = prompt("digite o valor de B")
+var c = prompt("digite o valor de C")
+
+var delta = (b*b) - (4*a*c)
+var bask1 = (-b+Math.sqrt(delta))/2*a
+var bask2 = (-b-Math.sqrt(delta))/2*a
+
+alert("seu delta é: "+ delta)
+alert("X1(+) é: "+bask1)
+alert("X2(-) é: "+ bask2)*/
+
+
+
+/*var nome = prompt("Qual o seu nome?")
+var idade = prompt("Qual sua idade?")
+var cidade = prompt("Qual sua cidade?")
+var estado = prompt("Qual o seu Estado?")
+var numero = prompt("Escolha um Número")
+var ano = 2021 - idade
+var dobro = parseInt(numero) + parseInt(numero)
+
+
+alert("Agora vou repassa-los")
+alert("Olá, " + nome + "!")
+alert("Sua idade é " + idade)
+alert("Sua cidade é " + cidade)
+alert("Seu estado é " + estado)
+alert("O número que você escolheu foi " + numero) 
+alert("E o Dobro dele é " + dobro)
+alert("E o ano que você nasceu é " + ano)
+prompt("Tudo certo? \n Digite Sim")
+alert("Tchau, Brigado !")*/
+
+
+/*let idade = prompt("Qual sua idade?");
+
+if (idade >= 75) {
+  alert("bem vindo senhor");
+  alert("Recomendamos beber com moderação");
+  alert("em que posso te ajudar?");
+} else if (idade >= 18) {
+  alert("bem vindo a loja");
+  alert("em que posso te ajudar?");
+} else if (idade < 18) {
+  alert("você nao tem idade pra acessar nossa loja");
+  alert("ficaremos felizes em te atender quando você for maior de idade");
+} else alert("o campo nao foi preenchido corretamente");
+*/
+
+
+
